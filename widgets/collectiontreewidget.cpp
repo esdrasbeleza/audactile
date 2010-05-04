c4a2a3ebfd167ec7ac32e079fb4652da687aef1a
Esdras Beleza de Noronha <esdras@atlantico2388l.(none)>
2010-05-04 17:04:11 -0300

    Qt

---
+++ widgets/collectiontreewidget.cpp
@@ -0,0 +1,5 @@
+#include "collectiontreewidget.h"
+
+CollectionTreeWidget::CollectionTreeWidget()
+{
+}
---
+++ widgets/collectiontreewidget.h
@@ -0,0 +1,12 @@
+#ifndef COLLECTIONTREEWIDGET_H
+#define COLLECTIONTREEWIDGET_H
+
+#include <QTreeWidget>
+
+class CollectionTreeWidget : public QTreeWidget
+{
+public:
+    CollectionTreeWidget();
+};
+
+#endif // COLLECTIONTREEWIDGET_H
