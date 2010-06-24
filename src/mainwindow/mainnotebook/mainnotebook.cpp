dfb807df2da4e93be9e7bc3f521ce8a980b12dd3
Esdras Beleza <linux@esdrasbeleza.com>
2010-06-24 16:47:09 -0300

    Closes #4.

--- src/mainwindow/mainnotebook/mainnotebook.cpp
+++ src/mainwindow/mainnotebook/mainnotebook.cpp
@@ -14,18 +14,15 @@ MainNotebook::MainNotebook(QWidget *parent)
     // Widget of the Files tab
     FilesystemWidget *filesystemWidget = new FilesystemWidget(this);
 
-
     // Creates a label test widget
     QLabel *testLabel2 = new QLabel(tr("Test label 2"));
     QLabel *testLabel3 = new QLabel(tr("Test label 3"));
 
     // Creates one tab.
-    addTab(collectionWidget, tr("Collection"));
-    addTab(filesystemWidget, tr("Files"));
-    addTab(testLabel2, tr("Context"));
-    addTab(testLabel3, tr("Playlists"));
-
-
+    addTab(collectionWidget, QIcon::fromTheme("audio-x-generic"), tr("Collection"));
+    addTab(filesystemWidget, QIcon::fromTheme("system-file-manager"), tr("Files"));
+    addTab(testLabel2, QIcon::fromTheme("emblem-web"), tr("Context"));
+    addTab(testLabel3, QIcon::fromTheme("text-x-generic"), tr("Playlists"));
 
     show();
 }
